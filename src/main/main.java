e7fe819b7067709f6cacad073f5b87569fd34f4d
Brice JEANNE <brice.jeanne@etu.univ-amu.fr>
2017-03-31 15:17:15 +0200

    Base project created

---
+++ src/main/main.java
@@ -0,0 +1,13 @@
+/*
+    Created by Brice JEANNE
+    31/03/2017
+*/
+
+package main;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("mes couilles");
+    }
+}
